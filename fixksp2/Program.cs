9dfba4cb01ac06441535ccc649b8eb2887fef9f5
Nathan Peake <37817019+NathanPeake@users.noreply.github.com>
2023-09-24 22:24:13 +0100

    Downgrade to net472

--- fixksp2/Program.cs
+++ fixksp2/Program.cs
@@ -1,3 +1,19 @@
 ﻿using Microsoft.Win32;
+using System;
 
-Registry.CurrentUser.DeleteSubKeyTree(@"Software\Intercept Games\Kerbal Space Program 2");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Registry.CurrentUser.DeleteSubKeyTree(@"SOFTWARE\Intercept Games\Kerbal Space Program 2");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        Console.WriteLine("you can now run ksp2 again");
+        Console.ReadLine();
+    }
+}
